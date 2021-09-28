9a08f4eba54ee0ba39a0363e981b638647130b70
ck-linuxx <you@example.com>
2021-09-27 21:29:42 -0300

    master

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { useParams } from "react-router-dom"
 
 import Button from "./Button"
+import "./TaskDetails.css"
 
 const TaskDetails = () => {
     const params = useParams()
