282e2097d6cdcff0c4caa518b7ee439c0f5503b8
ck-linuxx <you@example.com>
2021-09-28 19:57:20 -0300

    master

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ const App = () =>{
   useEffect(() => {
     const fetchTasks = async() => {
       const {data} = await axios.get("https://jsonplaceholder.cypress.io/todos?_limit=10")
+      setTasks(data)
     }
 
     fetchTasks()
