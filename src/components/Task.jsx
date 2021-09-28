5d0c1d3a86549ec480d306424dcb95f2ae55a610
ck-linuxx <you@example.com>
2021-09-27 21:41:15 -0300

    master

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { CgClose, CgInfo } from "react-icons/cg"
+import {useHistory} from "react-router-dom"
+
 import "./Task.css"
 
 const Task = ({ task, handleTaskClick, handleTaskDeletion}) => {
+    const history = useHistory()
+
+    const handleTaskDetailsClick = () => {
+        history.push(`/${task.title}`)
+    }
+
     return (
         <div className="task-container" style={task.completed ? { borderLeft: "6px solid chartreuse" } : {}}>
 
@@ -11,7 +19,7 @@ const Task = ({ task, handleTaskClick, handleTaskDeletion}) => {
             </div>
 
             <div className="buttons-container">
-                <button className="see-task-details-button"><CgInfo/></button>
+                <button className="see-task-details-button" onClick={handleTaskDetailsClick}><CgInfo/></button>
                 <button className="remove-task-button" onClick={() => handleTaskDeletion(task.id)}><CgClose/></button>
             </div>
         </div>
